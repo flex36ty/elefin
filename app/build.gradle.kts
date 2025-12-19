4fda74be5360a59a68bdc1a0834d6d7a3593a813
flex36ty <flex360@gmail.com>
2025-12-19 12:32:49 -0500

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         targetSdk = 36
 
         // Version code: major * 10000 + minor * 100 + patch
-        versionCode = 10117
-        versionName = "1.1.17"
+        versionCode = 10118
+        versionName = "1.1.18"
 
         ndk {
             abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64"))
