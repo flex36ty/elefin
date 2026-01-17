06f3387fa4b3f14c0e4e521abff988a931784d0f
flex36ty <flex360@gmail.com>
2026-01-17 09:48:03 -0500

    Customizable Row Card Count

--- decoder_av1/build/intermediates/cxx/Release/1ap1i714/logs/armeabi-v7a/build_command_gav1JNI.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\flex360\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\flex360\\AndroidStudioProjects\\elefin\\decoder_av1\\.cxx\\Release\\1ap1i714\\armeabi-v7a" ^
-  gav1JNI
