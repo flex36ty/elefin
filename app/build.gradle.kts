8334940310a1f871ee3da959a0e77e896267322f
flex36ty <flex360@gmail.com>
2026-02-10 16:23:39 -0500

    1.1.24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         targetSdk = 36
 
         // Version code: major * 10000 + minor * 100 + patch
-        versionCode = 10123
-        versionName = "1.1.23"
+        versionCode = 10124
+        versionName = "1.1.24"
 
         ndk {
             abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64"))
